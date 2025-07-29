c2052e8fb3f7a3ddd2f3a6360e33e11695c21998
StefanHarads <info@forstarbeiten.com>
2025-07-29 17:13:35 +0200

    Passwortlänge

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
 
 class Tree(db.Model):
     id = db.Column(db.Integer, primary_key=True)

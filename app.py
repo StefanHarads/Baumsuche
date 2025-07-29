006ae5214eea1f59747d0a201fe8d89ee8ba38d3
StefanHarads <info@forstarbeiten.com>
2025-07-29 13:31:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import os

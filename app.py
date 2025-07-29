927a2e5eaf08c5f3df8d1736b03bc3db04160ad3
StefanHarads <info@forstarbeiten.com>
2025-07-29 17:02:34 +0200

    Endlich

--- app.py
+++ app.py
@@ -144,8 +144,6 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
- from sqlalchemy import text
-
 @app.route('/debug-tables')
 def debug_tables():
     try:
